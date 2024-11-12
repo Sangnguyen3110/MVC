0e415957fc6020a40bfc47426a19bf43826afa38
Sangnguyen3110 <nguyenhuusang31102002@gmail.com>
2024-11-12 11:08:22 +0700

    11/11/2024

---
+++ Lesson03.mvc/Lesson03.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson03.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
